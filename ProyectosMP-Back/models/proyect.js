a121cec4418a421645c27b87fb6b6dc51614013c
Fernando Gamba <45173829+FernandoGamba@users.noreply.github.com>
2019-08-23 08:14:24 -0600

    Modelo proyecto bbsita

--- ProyectosMP-Back/models/proyect.js
+++ ProyectosMP-Back/models/proyect.js
@@ -5,9 +5,23 @@ var Schema = mongoose.Schema;
 
 var proyectSchema = Schema({
     _id: String,
+    responsability: String,
+    priorityOfDocument: Number,
+    priorityToday: Number,
     company: {type: Schema.ObjectId, ref: 'company'},
+    country: String,
     module: {type: Schema.ObjectId, ref: 'module'},
-    status: String
+    dateOfRequest: Date,
+    dateOfStart: Date,
+    whoAskFor: String,
+    percentageOfProgrss: String,
+    dateLimit: Date,
+    remainingDays: Number,
+    dateOfDelivery: Date,
+    effectiveDays: String,
+    description: String,
+    status: String,
+    countersOfStatus: String
 })
 
 module.exports = mongoose.model('proyect', proyectSchema);
